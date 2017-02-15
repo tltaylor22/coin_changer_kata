e415fcff3cd5a6766974b43484c82c8368daa27f
Tim Taylor <ttaylor@clarion.edu>
2017-02-15 16:33:33 -0500

    trying to figure out the math and not having much luck...google time!

--- coin_changer.rb
+++ coin_changer.rb
@@ -1,11 +1,19 @@
 def change(amount)
 
-	return [25] * (amount / 25) if amount > 24
+	exact_change = []
+	{[25, 10, 5, 1].each do |coin|
+		exact_change << [coin] * (amount / coin) if amount >= coin
+		amount = amount - (amount / coin)
 
-	return [10] * (amount / 10) if amount > 9
+	end
 
-	return [5] * (amount / 5) if amount > 4
-
-	[1] * amount
-	
+	exact_change
 end
+
+	# return [25] * (amount / 25) if amount > 24
+
+	# return [10] * (amount / 10) if amount > 9
+
+	# return [5] * (amount / 5) if amount > 4
+
+	# [1] * amount
